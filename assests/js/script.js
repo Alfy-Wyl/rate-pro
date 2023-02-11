302e5ebc14ce2d245e83d8bd25e0668b70555d18
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-02-11 16:02:05 +0000

    declared variables in UI

---
+++ assests/js/script.js
@@ -0,0 +1,8 @@
+// Declare Variables for UI elements
+const baseCurrency = document.getElementById('first-currency');
+const firstAmount = document.getElementById('first-amount');
+const compareCurrency = document.getElementById('second-currency');
+const secondAmount = document.getElementById('second-amount');
+const switchEl = document.getElementById('switch')
+const exchangeRate = document.getElementById('exchange-rate');
+
