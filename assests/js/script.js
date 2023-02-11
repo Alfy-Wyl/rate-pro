df76a3e3562b7928be8692c7fcb9b4f0500ab4b3
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-02-11 22:16:41 +0000

    added event listeners for the Select and Input elements

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,6 +3,18 @@ const baseCurrency = document.getElementById('first-currency');
 const firstAmount = document.getElementById('first-amount');
 const compareCurrency = document.getElementById('second-currency');
 const secondAmount = document.getElementById('second-amount');
-const switchEl = document.getElementById('switch')
+const switchEl = document.getElementById('switch');
 const exchangeRate = document.getElementById('exchange-rate');
 
+// Function to Fetch Exchange Rate from API
+function calculate () {
+
+}  
+
+
+
+// Add Event Listeners for the Select and Input Elements
+baseCurrency.addEventListener('change', calculate);
+firstAmount.addEventListener('input', calculate);
+compareCurrency.addEventListener('change', calculate);
+secondAmount.addEventListener('input', calculate);
